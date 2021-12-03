94f81ecb97a475cc7bd755b59454829ec652e133
GeheimagentNr1 <geheimelache@hotmail.com>
2021-12-03 23:30:26 +0100

    Ported to 1.18

--- src/main/java/de/geheimagentnr1/moremobgriefingoptions/MoreMobGriefingOptions.java
+++ src/main/java/de/geheimagentnr1/moremobgriefingoptions/MoreMobGriefingOptions.java
@@ -3,7 +3,7 @@ package de.geheimagentnr1.moremobgriefingoptions;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
+import net.minecraftforge.network.NetworkConstants;
 
 
 @SuppressWarnings( "UtilityClassWithPublicConstructor" )
@@ -19,7 +19,7 @@ public class MoreMobGriefingOptions {
 		ModLoadingContext.get().registerExtensionPoint(
 			IExtensionPoint.DisplayTest.class,
 			() -> new IExtensionPoint.DisplayTest(
-				() -> FMLNetworkConstants.IGNORESERVERONLY,
+				() -> NetworkConstants.IGNORESERVERONLY,
 				( remote, isServer ) -> true
 			)
 		);
