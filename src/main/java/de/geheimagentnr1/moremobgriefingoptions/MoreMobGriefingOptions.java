62c147d567e707fa566460c46465a585b348e75a
GeheimagentNr1 <geheimelache@hotmail.com>
2022-06-12 12:10:17 +0200

    Ported to 1.19

--- src/main/java/de/geheimagentnr1/moremobgriefingoptions/MoreMobGriefingOptions.java
+++ src/main/java/de/geheimagentnr1/moremobgriefingoptions/MoreMobGriefingOptions.java
@@ -1,9 +1,18 @@
 package de.geheimagentnr1.moremobgriefingoptions;
 
+import de.geheimagentnr1.moremobgriefingoptions.elements.commands.arguments.config_option.ConfigOptionArgument;
+import de.geheimagentnr1.moremobgriefingoptions.elements.commands.arguments.mob_griefing_option.MobGriefingOptionArgument;
+import net.minecraft.commands.synchronization.ArgumentTypeInfo;
+import net.minecraft.commands.synchronization.ArgumentTypeInfos;
+import net.minecraft.commands.synchronization.SingletonArgumentInfo;
+import net.minecraft.core.Registry;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.network.NetworkConstants;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
 
 
 @SuppressWarnings( "UtilityClassWithPublicConstructor" )
@@ -12,6 +21,31 @@ public class MoreMobGriefingOptions {
 	
 	
 	public static final String MODID = "moremobgriefingoptions";
+	
+	private static final DeferredRegister<ArgumentTypeInfo<?, ?>> COMMAND_ARGUMENT_TYPES = DeferredRegister.create(
+		Registry.COMMAND_ARGUMENT_TYPE_REGISTRY,
+		MODID
+	);
+	
+	private static final RegistryObject<SingletonArgumentInfo<ConfigOptionArgument>>
+		CONFIG_OPTION_COMMAND_ARGUMENT_TYPE =
+		COMMAND_ARGUMENT_TYPES.register(
+			ConfigOptionArgument.registry_name,
+			() -> ArgumentTypeInfos.registerByClass(
+				ConfigOptionArgument.class,
+				SingletonArgumentInfo.contextFree( ConfigOptionArgument::config_option )
+			)
+		);
+	
+	private static final RegistryObject<SingletonArgumentInfo<MobGriefingOptionArgument>>
+		MOB_GRIEFING_OPTION_COMMAND_ARGUMENT_TYPE =
+		COMMAND_ARGUMENT_TYPES.register(
+			MobGriefingOptionArgument.registry_name,
+			() -> ArgumentTypeInfos.registerByClass(
+				MobGriefingOptionArgument.class,
+				SingletonArgumentInfo.contextFree( MobGriefingOptionArgument::mob_griefing_option )
+			)
+		);
 	
 	@SuppressWarnings( "unused" )
 	public MoreMobGriefingOptions() {
@@ -23,5 +57,6 @@ public class MoreMobGriefingOptions {
 				( remote, isServer ) -> true
 			)
 		);
+		COMMAND_ARGUMENT_TYPES.register( FMLJavaModLoadingContext.get().getModEventBus());
 	}
 }
--- src/main/java/de/geheimagentnr1/moremobgriefingoptions/elements/commands/arguments/ModArgumentTypes.java
+++ src/main/java/de/geheimagentnr1/moremobgriefingoptions/elements/commands/arguments/ModArgumentTypes.java
@@ -13,12 +13,12 @@ public class ModArgumentTypes {
 	
 	public static void registerArgumentTypes() {
 		
-		Registry.register(
+		/*Registry.register(
 			Registry.COMMAND_ARGUMENT_TYPE,
 			MoreMobGriefingOptions.MODID + ":" + ConfigOptionArgument.registry_name,
 			ArgumentTypeInfos.registerByClass(
 				ConfigOptionArgument.class,
-				SingletonArgumentInfo.contextFree( ConfigOptionArgument::new )
+				SingletonArgumentInfo.contextFree( ConfigOptionArgument::config_option )
 			)
 		);
 		Registry.register(
@@ -26,8 +26,8 @@ public class ModArgumentTypes {
 			MoreMobGriefingOptions.MODID + ":" + MobGriefingOptionArgument.registry_name,
 			ArgumentTypeInfos.registerByClass(
 				MobGriefingOptionArgument.class,
-				SingletonArgumentInfo.contextFree( MobGriefingOptionArgument::new )
+				SingletonArgumentInfo.contextFree( MobGriefingOptionArgument::mob_griefing_option )
 			)
-		);
+		);*/
 	}
 }
